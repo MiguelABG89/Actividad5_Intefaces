0fc3e88179291c91f91196842056e07c816e76e4
Alumno_FP <150008@iessanalberto.com>
2023-11-07 13:07:46 +0100

    commit fin dos UserEffect

--- src/componentes/ListaUsu.js
+++ src/componentes/ListaUsu.js
@@ -7,28 +7,29 @@ async function fetchPosts() {
     const blogPosts = await response.json();
     return blogPosts;
 }
-  
+
 function ListaUsu(){
+    async function fetchPosts2() {
+        const response2 = await fetch("http://localhost:5000/users/"+ selectedUser);
+        const blogPosts2 = await response2.json();
+        return blogPosts2;
+    }
+
     const [data, setData] = useState([]);
     const [selectedUser, setSelectedUser] = useState();
-    const [id, setid] = useState(false);  
+    const [DatosUser, setDatosUser] = useState();
+    
+
 
     useEffect(function(){
         fetchPosts().then((fetchPosts)=>setData(fetchPosts));
     },[])
-    useEffect(function() {
-        fetchPosts().then((fetchPosts)=>setData(fetchPosts));
-    }, [selectedUser]);
 
-    const handleMostrarDatosClick = (user, id) => {
-        if (selectedUser && selectedUser.id === user.id) {
-            setSelectedUser(null);
-            setid(id);
-        } else {
-            setSelectedUser(user);
-            setid(id);
-        }
-    };
+    useEffect(function(){
+        fetchPosts2().then((fetchPosts2)=>{setDatosUser(fetchPosts2) }
+        );
+    },[selectedUser])
+
 
     return(
         <div>
@@ -36,8 +37,16 @@ function ListaUsu(){
                 <li>
                     {user.name}
                     <span>        </span>
-                <button onClick={()=>handleMostrarDatosClick(user, user.id)}
-                >Mostrar datos</button>
+                    <button onClick={()=>setSelectedUser(user.id)}>Mostrar datos</button>
+                    {DatosUser && DatosUser.id === user.id && (
+                        <div>
+                            <p>Detalles del usuario:</p>
+                            <p>{DatosUser.name}</p>
+                            <p>{DatosUser.phone}</p>
+                            <p>{DatosUser.email}</p>
+                        
+                        </div>
+                    )}
                 </li>
              ))
             }
